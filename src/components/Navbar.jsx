2773bb5014d13abf0d7d8fd36bc4f05d714edb13
Julien <116632934+JulienQ1@users.noreply.github.com>
2026-07-01 20:47:08 +0200

    Update job title in Navbar component to French

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
       alt='logo'
       className='w-9 h-9 object-contain'/>
       <p className='text-white text-[18px] font-bold cursor-pointer flex'>Julien &nbsp;
-      <span className='sm:block hidden'>| &nbsp; Research engineer</span></p>
+      <span className='sm:block hidden'>| &nbsp; Ingénieur de Recherche</span></p>
     </Link>
     <ul className='list-none hidden sm:flex flex-row gap-10'>
     {navLinks.map((Link) => (
