1a65fa4238bb8d5afd061608652fe642adea6b7c
Julien <116632934+JulienQ1@users.noreply.github.com>
2026-07-01 20:32:14 +0200

    Refactor constants and update project details
    Updated navigation links and experience entries with new titles and details. Added new projects and modified existing project descriptions.

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,20 +20,24 @@ import {
     python,
     ift,
     anatomy,
+    Esilv,
+    iim,
+    gb,
+    sk,
     
   } from "../assets";
   
   export const navLinks = [
     {
-      id: "about",
+      id: "About",
       title: "About",
     },
     {
-      id: "work",
-      title: "Work",
+      id: "Works",
+      title: "Works",
     },
     {
-      id: "contact",
+      id: "Contact",
       title: "Contact",
     },
   ];
@@ -52,7 +56,7 @@ import {
       icon: backend,
     },
     {
-      title: "Muscle Sensor",
+      title: "Capteur EMG",
       icon: creator,
     },
   ];
@@ -109,28 +113,97 @@ import {
   ];
   
   const experiences = [
+    
+        {
+      title: "ESILV",
+      company_name: "Master of Science «Innovation & Creative Technology »",
+      icon: Esilv,
+      iconBg: "#383E56",
+      date: "Septembre 2023 - 2026",
+      points: [
+        "Augmented Reality, Computer Vision, Motion Capture",
+      
+      ],
+    },
     {
-      title: "Emg-fullbody",
-      company_name: "Creative Technologie",
+      title: "Instutute for Future Technologies",
+      company_name: "Étudiant de recherche dans le domaine de l'apprentissage humain (Human Learning)",
       icon: ift,
       iconBg: "#383E56",
-      date: "September 2023 - current",
+      date: "Septembre 2023 - 2025",
       points: [
-        "Make computer interaction more Healthier",
-        "Make the interaction with computer more accessible",
-        "Create new way of interact with computer.",
+        "Spécialisation : « Apprentissage humain » — Interaction homme-machine, génie logiciel",
+        
+        ,
+      
+      ],
+    },
+       {
+      title: "institut of internet and multimedia - IIM",
+      company_name: "Digital Master Bootcamp",
+      icon: iim,
+      iconBg: "#383E56",
+      date: "Septembre 2022 - 2023",
+      points: [
+        " UX/UI Design, Motion Design (Adobe Suite), Développement Web, Gestion de Projet",
+        
+        ,
       
       ],
     },
   ];
-  
+
  
   
   const projects = [
     {
+      name: "2D-EMG-GAME",
+      description:
+        "Une visualisation 2D interactive du système musculaire humain",
+      tags: [
+        {
+          name: "biomedical",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "OpenSignal",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Unity",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: anatomy,
+      source_code_link: "https://github.com/JulienQ1/2D-EMG-GAME",
+    },
+    {
+      name: "IFTendo-Battleship",
+      description:
+        "Le jeu « Battleship », écrit en C++ dans l'IDE Arduino. Grâce à un hôte et un client, ce jeu se joue à deux. L'hôte est connecté à une matrice LED 8x8 et le client à une matrice de boutons 8x8. Les joueurs peuvent jouer à l'aide des boutons et les LED affichent le résultat de leurs actions.",
+        
+      tags: [
+        {
+          name: "C++",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "arduino",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Kicad",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: gb,
+      source_code_link: "https://github.com/JulienQ1/IFTendo-Battleship",
+    },
+    {
       name: "Muscular System Visualization",
       description:
-        "An interactive 3D visualization of the human muscular system",
+        "Une visualisation 3D interactive du système musculaire humain",
+        
       tags: [
         {
           name: "biomedical",
@@ -145,8 +218,8 @@ import {
           color: "pink-text-gradient",
         },
       ],
-      image: anatomy,
-      source_code_link: "https://www.julienquenneville.com",
+      image: sk,
+      source_code_link: "https://github.com/Z-Anatomy/Unity-app_Z-Anatomy",
     },
     
   ];
